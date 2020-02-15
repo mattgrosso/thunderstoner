93777140d222d6f77a86b0e5b0c8c7c333ef7f5e
Matt Grosso <mattgrosso@gmail.com>
2020-02-15 10:00:14 -0500

    Adds dummy data

--- src/assets/cards.js
+++ src/assets/cards.js
@@ -22,6 +22,52 @@ export const cards = {
         points: 4,
         set: "ToR"
       }
+    ],
+    test1: [
+      {
+        title: "Hellhound",
+        group: "Burnmarked",
+        traits: [
+          "Burnmarked",
+          "Fire",
+          "Level 1"
+        ],
+        level: 2,
+        health: 7,
+        goldValue: 2,
+        battles: [
+          "Gain on curse. Each other player must discard 1 hero, or show a hand with none."
+        ],
+        aftermaths: [
+          "If you did not defeat this card, gain 1 curse."
+        ],
+        xp: 2,
+        points: 4,
+        set: "ToR"
+      }
+    ],
+    test2: [
+      {
+        title: "Hellhound",
+        group: "Burnmarked",
+        traits: [
+          "Burnmarked",
+          "Fire",
+          "Level 1"
+        ],
+        level: 2,
+        health: 7,
+        goldValue: 2,
+        battles: [
+          "Gain on curse. Each other player must discard 1 hero, or show a hand with none."
+        ],
+        aftermaths: [
+          "If you did not defeat this card, gain 1 curse."
+        ],
+        xp: 2,
+        points: 4,
+        set: "ToR"
+      }
     ]
   },
   heroes: {
@@ -118,10 +164,198 @@ export const cards = {
         points: 5,
         set: "ItA"
       }
+    },
+    testA: {
+      level1: {
+        title: "TestA Neophyte",
+        group: "TestA",
+        traits: [
+          "Human",
+          "Fighter",
+          "Cleric",
+          "Level 1"
+        ],
+        level: 1,
+        goldValue: 0,
+        strength: 4,
+        physicalAttack: 2,
+        magicAttack: 0,
+        villages: [],
+        dungeons: [
+          "Repeat. Destroy 1 Light item to add Magic Attack +3"
+        ],
+        aftermaths: [],
+        xp: 2,
+        points: 0,
+        set: "ItA"
+      },
+      level2: {
+        title: "TestA Monk",
+        group: "TestA",
+        traits: [
+          "Human",
+          "Fighter",
+          "Cleric",
+          "Level 2"
+        ],
+        level: 2,
+        goldValue: 0,
+        strength: 4,
+        physicalAttack: 3,
+        magicAttack: 0,
+        villages: [],
+        dungeons: [
+          "Repeat. Destroy 1 Light item to add Magic Attack +4"
+        ],
+        aftermaths: [],
+        xp: 3,
+        points: 0,
+        set: "ItA"
+      },
+      level3: {
+        title: "TestA Grandmaster",
+        group: "TestA",
+        traits: [
+          "Human",
+          "Fighter",
+          "Cleric",
+          "Level 3"
+        ],
+        level: 3,
+        goldValue: 0,
+        strength: 5,
+        physicalAttack: 3,
+        magicAttack: 0,
+        villages: [],
+        aftermaths: [],
+        specialEffects: [
+          "Ignore Darkness penalties.",
+          "Add Magic Attack +3 if no Light items are present."
+        ],
+        points: 3,
+        set: "ItA"
+      },
+      level4: {
+        title: "TestA Shihan",
+        group: "TestA",
+        traits: [
+          "Human",
+          "Fighter",
+          "Cleric",
+          "Level 4"
+        ],
+        level: 4,
+        goldValue: 0,
+        strength: 6,
+        physicalAttack: 4,
+        magicAttack: 0,
+        villages: [],
+        aftermaths: [],
+        specialEffects: [
+          "Ignore Darkness penalties.",
+          "Add Magic Attack +4 and ignore Battle effects if no Light items are present."
+        ],
+        points: 5,
+        set: "ItA"
+      }
+    },
+    testB: {
+      level1: {
+        title: "TestB Neophyte",
+        group: "TestB",
+        traits: [
+          "Human",
+          "Fighter",
+          "Cleric",
+          "Level 1"
+        ],
+        level: 1,
+        goldValue: 0,
+        strength: 4,
+        physicalAttack: 2,
+        magicAttack: 0,
+        villages: [],
+        dungeons: [
+          "Repeat. Destroy 1 Light item to add Magic Attack +3"
+        ],
+        aftermaths: [],
+        xp: 2,
+        points: 0,
+        set: "ItA"
+      },
+      level2: {
+        title: "TestB Monk",
+        group: "TestB",
+        traits: [
+          "Human",
+          "Fighter",
+          "Cleric",
+          "Level 2"
+        ],
+        level: 2,
+        goldValue: 0,
+        strength: 4,
+        physicalAttack: 3,
+        magicAttack: 0,
+        villages: [],
+        dungeons: [
+          "Repeat. Destroy 1 Light item to add Magic Attack +4"
+        ],
+        aftermaths: [],
+        xp: 3,
+        points: 0,
+        set: "ItA"
+      },
+      level3: {
+        title: "TestB Grandmaster",
+        group: "TestB",
+        traits: [
+          "Human",
+          "Fighter",
+          "Cleric",
+          "Level 3"
+        ],
+        level: 3,
+        goldValue: 0,
+        strength: 5,
+        physicalAttack: 3,
+        magicAttack: 0,
+        villages: [],
+        aftermaths: [],
+        specialEffects: [
+          "Ignore Darkness penalties.",
+          "Add Magic Attack +3 if no Light items are present."
+        ],
+        points: 3,
+        set: "ItA"
+      },
+      level4: {
+        title: "TestB Shihan",
+        group: "TestB",
+        traits: [
+          "Human",
+          "Fighter",
+          "Cleric",
+          "Level 4"
+        ],
+        level: 4,
+        goldValue: 0,
+        strength: 6,
+        physicalAttack: 4,
+        magicAttack: 0,
+        villages: [],
+        aftermaths: [],
+        specialEffects: [
+          "Ignore Darkness penalties.",
+          "Add Magic Attack +4 and ignore Battle effects if no Light items are present."
+        ],
+        points: 5,
+        set: "ItA"
+      }
     }
   },
-  villages: {
-    villagers: {
+  villages: [
+    {
       title: "Bio-salvager",
       traits: [
         "Villager",
@@ -141,6 +375,48 @@ export const cards = {
       points: 0,
       set: "Numenera",
       number: 8
+    },
+    {
+      title: "TestUno",
+      traits: [
+        "Villager",
+        "Mercenary"
+      ],
+      cost: 4,
+      goldValue: 0,
+      aftermaths: [],
+      villages: [
+        "Draw 1 card. If it is a monster, draw 1 additional card."
+      ],
+      dungeons: [
+        "Draw 1 card. If it is a monster, draw 1 additional card.",
+        "Add Physical Attack +1 if a monster is present."
+      ],
+      xp: 0,
+      points: 0,
+      set: "Numenera",
+      number: 8
+    },
+    {
+      title: "TestDos",
+      traits: [
+        "Villager",
+        "Mercenary"
+      ],
+      cost: 4,
+      goldValue: 0,
+      aftermaths: [],
+      villages: [
+        "Draw 1 card. If it is a monster, draw 1 additional card."
+      ],
+      dungeons: [
+        "Draw 1 card. If it is a monster, draw 1 additional card.",
+        "Add Physical Attack +1 if a monster is present."
+      ],
+      xp: 0,
+      points: 0,
+      set: "Numenera",
+      number: 8
     }
-  }
+  ]
 }
